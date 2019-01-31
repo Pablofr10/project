946214eba3646e204162d64b3af6bbf3c9e04f5a
Pablo <rafanet.live@gmail.com>
2019-01-31 19:44:21 -0200

    This is my first commit!

---
+++ wordcount/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def count(request):
+    fulltext = request.GET['fulltext']
+    
+    wordlist = fulltext.split()
+
+    return render(request, 'count.html', {'fulltext':fulltext, 'count':len(wordlist)})
+
+def about(request):
+    nasc = '19/10/1994'
+    return render(request, 'about.html', {'name':'Pablo', 'nasc': nasc})
